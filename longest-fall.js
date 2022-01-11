64dee060d8c97ac247917a421e3ffd9bbd83d5a1
Virginia Wu <virginiawu11@gmail.com>
2022-01-10 20:42:31 -0500

    complete further study

--- longest-fall.js
+++ longest-fall.js
@@ -1,4 +1,19 @@
 // add whatever parameters you deem necessary
-function longestFall() {}
+function longestFall(arr) {
+  let longestCount = 1;
+  let currentCount = 1;
+  if (arr.length === 0) return 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      currentCount++;
+      if (currentCount > longestCount) {
+        longestCount = currentCount;
+      }
+    } else {
+      currentCount = 1;
+    }
+  }
+  return longestCount;
+}
 
 module.exports = longestFall;
--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {}
+function pivotIndex(arr) {
+  let leftSum = 0;
+  let rightSum = 0;
+  for (let i = 1; i < arr.length; i++) {
+    rightSum += arr[i];
+  }
+  for (let i = 0; i < arr.length - 1; i++) {
+    leftSum += arr[i];
+    rightSum -= arr[i + 1];
+    if (leftSum === rightSum) {
+      return i + 1;
+    }
+  }
+  return -1;
+}
 module.exports = pivotIndex;
